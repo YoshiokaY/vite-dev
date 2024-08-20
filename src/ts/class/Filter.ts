f8ae326cd53cd4f0d4edcc383717ca7773b2a0a0
YoshiokaY <yoshioka.y@nadia.bz>
2024-08-20 19:18:22 +0900

    [update]AND検索処理

--- src/ts/class/Filter.ts
+++ src/ts/class/Filter.ts
@@ -1,49 +1,30 @@
 //** フィルター */
-import { html, css, LitElement } from "lit";
+import { html, LitElement } from "lit";
 import { property, customElement } from "lit/decorators.js";
 @customElement("filter-list")
 export class Filter extends LitElement {
   @property({ type: Array })
   category = []; // カテゴリー
   @property()
-  list: number = 0; // 一行の数
-  @property()
   all?: string; // すべて選択必要な場合
   @property()
-  visible?: number; // 初期表示
+  visible?: number; // 表示件数
+  // 選択されたキーワードを保存する配列
+  private _filterCat: string[] = [];
+  private SHOW_CLASS_NAME = "-visible"; // 表示用クラス
+  private ALL_CARD_LIST = this.querySelectorAll(".c_filter_item");
+  private MORE_BTN = this.querySelector(".c_filter_more"); // もっと見るボタン
+  private targetElements: Element[] = []; // slotをLigth DOMに格納
 
-  /**
-   * カスタムエレメントからslotを取得
-   * @param children target children
-   * @returns Element[] slot elements
-   */
-  private static findSlots(children: HTMLCollection): Element[] {
-    let slots: Element[] = [];
-    for (const child of children) {
-      if (child.tagName === "SLOT") {
-        slots.push(child);
-      } else if (child.tagName.indexOf("-") < 0) {
-        slots = slots.concat(this.findSlots(child.children));
-      }
-    }
-    return slots;
-  }
-
-  /**
-   * slotをライトDOMに挿入
-   */
-  private targetElements: Element[] = [];
-
+  // DOM描画
   override connectedCallback() {
     super.connectedCallback();
-
     this.targetElements = [...this.children];
   }
 
-  /**
-   * 初期描画
-   */
+  // 初期描画
   override firstUpdated() {
+    // slotがある場合Light DOMに挿入する
     const slots = Filter.findSlots(this.children);
     for (const child of this.targetElements) {
       let targetSlot = null;
@@ -59,137 +40,164 @@ export class Filter extends LitElement {
       }
     }
     this.targetElements = [];
+    // すべて表示の選択肢がある場合、初期選択状態にする
+    this._setCatAll();
   }
 
-  // -----------------------------------------
-  // 職種ラジオボタンクリック時のイベントハンドラ
-  // -----------------------------------------
-  _handleClick(e: Event) {
-    const SHOWN_CLASS_NAME = "-visible";
-    let target = e.target as HTMLInputElement;
-    let cateStr = target.checked ? target.value : ""; // チェック状態のvalue値
-    const _allCardList = this.querySelectorAll(".c_filter_item"); // フィルタリングする要素
-    const FIRST_SHOW_NUM = this.visible ? this.visible : _allCardList.length;
-    let _cateCardList: any = _allCardList; // 最初は「すべて」カテゴリ選択状態
-    let _shownCardNum: number = 0; // 表示中の件数は0
-    const MORE_SHOW_NUM = 3;
-
-    if (!_allCardList) {
-      // データがない場合は何もしない
-      return;
-    }
-
-    if (cateStr === this.all) {
-      // 選択したカテゴリーがすべての場合の他のチェックボックスを外す
-      const checkbox = this.querySelectorAll("input");
-      checkbox.forEach((input, i) => {
-        if (i !== 0) {
-          input.checked = false;
-        }
-      });
-    } else {
-      // カテゴリーがすべてのチェックボックスを外す
-      const checkAll = this.querySelector("#cat0") as HTMLInputElement;
-      if (checkAll) {
-        checkAll.checked = false;
-      }
-      // 選択したカテゴリの一覧を取得
-      _cateCardList = Array.from(_allCardList).filter((card: any) => {
-        const category = card.querySelector(".cat");
-        if (category) {
-          const cateText = category.textContent;
-          return cateText === cateStr;
-        } else {
-          return false;
-        }
-      });
-    }
-
-    // 表示中のデータを非表示
-    _allCardList.forEach((card) => {
-      card.classList.remove(SHOWN_CLASS_NAME);
-    });
-    // 選択したカテゴリのデータを表示
-    for (let i = 0; i < FIRST_SHOW_NUM && i < _cateCardList.length; i++) {
-      _cateCardList[i].classList.add(SHOWN_CLASS_NAME);
-      _shownCardNum++;
-    }
-
-    const _moreBtn: HTMLElement | null = this.querySelector(".c_filter_more"); // もっと見るボタン
-    // もっと見るボタンの表示切替
-    if (_moreBtn) {
-      console.log(_shownCardNum < _cateCardList.length);
-      if (_shownCardNum < _cateCardList.length) {
-        // 表示させる
-        _moreBtn.classList.add(SHOWN_CLASS_NAME);
-      } else {
-        // 非表示
-        _moreBtn.classList.remove(SHOWN_CLASS_NAME);
-      }
-    }
-    const catButtons = this.querySelectorAll("input");
-    if (catButtons.length > 0) {
-      // もっと見るボタン押下時の処理を登録
-      if (_moreBtn) {
-        _moreBtn.addEventListener("click", () => {
-          if (_cateCardList.length < 1) {
-            // データがない場合は何もしない
-            return;
-          }
-          // データを表示
-          const maxCount = _shownCardNum + MORE_SHOW_NUM; // 表示可能な最大件数
-          for (let i = _shownCardNum; i < maxCount && i < _cateCardList.length; i++) {
-            _cateCardList[i].classList.add(SHOWN_CLASS_NAME);
-            _shownCardNum++;
-          }
-
-          // もっと見るボタンの表示切替
-          if (_moreBtn) {
-            if (_shownCardNum < _cateCardList.length) {
-              // 表示させる
-              _moreBtn.classList.add(SHOWN_CLASS_NAME);
-            } else {
-              // 非表示
-              _moreBtn.classList.remove(SHOWN_CLASS_NAME);
-            }
-          }
-        });
-      }
-
-      // 一番最初のカテゴリを選択状態にするためにclickイベントを発火
-      (<HTMLElement>catButtons[0]).click();
-    }
-  }
-
+  // DOM構造
   override render() {
     return html`
     <div class="c_filter">
-      <ul class="grid grid-cols-3">
+      <ul class="c_filter_list grid">
         ${
+          // 全件表示が必要な場合描画
           this.all
             ? html`
           <li>
-          <input type="checkbox" @click=${this._handleClick} name="filter" value=${this.all} id="cat0" />
-          <label for="cat0">${this.all}</label>
-        </li>
+            <input type="checkbox" @click=${this._handleClick} name="filter" value=${this.all} id="cat0" />
+            <label for="cat0">${this.all}</label>
+          </li>
         `
             : html``
         }
 
-      ${this.category.map(
-        (cat: string, i: number) => html`
-        <li>
-          <input type="checkbox" @click=${this._handleClick} name="filter" value=${cat} id="cat${i + 1}" />
-          <label for="cat${i + 1}">${cat}</label>
-        </li>
-        `
-      )}
+        ${
+          // categoryの分だけフィルタリングボタンを描画
+          this.category.map(
+            (cat: string, i: number) => html`
+            <li>
+              <input type="checkbox" @click=${this._handleClick} name="filter" value=${cat} id="cat${i + 1}" />
+              <label for="cat${i + 1}">${cat}</label>
+            </li>
+          `
+          )
+        }
       </ul>
       <slot name="content"></slot>
       <slot name="more"></slot>
     </div>
     `;
   }
+
+  // -----------------------------------------
+  // ボタンクリック時のイベントハンドラ
+  // -----------------------------------------
+  _handleClick(e: Event) {
+    let target = e.target as HTMLInputElement;
+    let keyword = target.value; // 選択カテゴリーのvalu値
+
+    if (!this.ALL_CARD_LIST) {
+      // データがない場合は何もしない
+      return;
+    }
+    // 全件表示のチェックボックスを外す
+    const checkAll = this.querySelector("#cat0") as HTMLInputElement;
+    if (checkAll) {
+      checkAll.checked = false;
+    }
+    // 選択中のvalueを配列に格納
+    if (target.checked) {
+      this._filterCat.push(keyword);
+    } else {
+      // 選択していない値を配列から除去
+      this._filterCat = this._filterCat.filter((k) => k !== keyword);
+    }
+    // チェックが一件もない場合、全件表示にする
+    if (this._filterCat.length === 0) {
+      this._setCatAll();
+    }
+    // フィルタリングを実行
+    this._filterElements();
+  }
+  // フィルタリング
+  _filterElements() {
+    let firstShowNum = this.visible ? this.visible : this.ALL_CARD_LIST.length; // 初期表示件数
+    let showCountNum: number = 0; // 現在表示している件数※初期は0件
+    let matchedCards: Element[] = []; // 一致した要素の配列
+    this.ALL_CARD_LIST.forEach((card) => {
+      // 表示中のデータを一旦非表示
+      card.classList.remove(this.SHOW_CLASS_NAME);
+      const cardText = card.textContent?.toLowerCase(); // 要素のカテゴリーを小文字で格納
+      const isMatch = this._filterCat.every((keyword) => cardText?.includes(keyword.toLowerCase())); // 現在選択中のカテゴリーと一致した場合trueを返す
+      // 一致するデータを配列に格納
+      if (isMatch) {
+        matchedCards.push(card);
+      }
+    });
+    // 選択したカテゴリーのデータを表示
+    for (let i = 0; i < firstShowNum && i < matchedCards.length; i++) {
+      matchedCards[i].classList.add(this.SHOW_CLASS_NAME);
+      showCountNum++;
+    }
+
+    // moreボタン
+    if (this.MORE_BTN && this.visible) {
+      this._setMoreBtn(showCountNum, matchedCards);
+    }
+  }
+
+  //
+  _setMoreBtn(showCountNum: number, matchedCards: any) {
+    let MORE_SHOW_NUM: number = this.visible ? this.visible : 0;
+    console.log(MORE_SHOW_NUM);
+    // moreボタンの表示/非表示
+    this._showMoreBtn(showCountNum, matchedCards.length);
+    this.MORE_BTN?.addEventListener("click", () => {
+      if (matchedCards.length < 1) {
+        // データがない場合は何もしない
+        return;
+      }
+      let maxCount = showCountNum + +MORE_SHOW_NUM; // 表示可能な最大件数
+      console.log(maxCount);
+      for (let i = showCountNum; i < maxCount && i < matchedCards.length; i++) {
+        matchedCards[i].classList.add(this.SHOW_CLASS_NAME);
+        showCountNum++;
+      }
+      // moreボタンの表示/非表示
+      this._showMoreBtn(showCountNum, matchedCards.length);
+    });
+  }
+
+  // moreボタンの表示/非表示
+  _showMoreBtn(showCountNum: number, matcheCardNum: number) {
+    if (showCountNum < matcheCardNum) {
+      // 表示させる
+      this.MORE_BTN?.classList.add(this.SHOW_CLASS_NAME);
+    } else {
+      // 非表示
+      this.MORE_BTN?.classList.remove(this.SHOW_CLASS_NAME);
+    }
+  }
+
+  // 全件表示
+  _setCatAll() {
+    const checkAll = this.querySelector("#cat0") as HTMLInputElement;
+    if (checkAll) {
+      // チェックをいれる
+      checkAll.checked = true;
+      // カスタムイベントでクリックしたことにする
+      (<HTMLElement>checkAll).click();
+    }
+  }
+
+  /**
+   * カスタムエレメントからslotを取得してLight DOMでも表示できるようにする
+   * @param children target children
+   * @returns Element[] slot elements
+   */
+  private static findSlots(children: HTMLCollection): Element[] {
+    let slots: Element[] = [];
+    for (const child of children) {
+      if (child.tagName === "SLOT") {
+        slots.push(child);
+      } else if (child.tagName.indexOf("-") < 0) {
+        slots = slots.concat(this.findSlots(child.children));
+      }
+    }
+    return slots;
+  }
+
+  // DOM描画
   protected createRenderRoot() {
     return this;
   }
