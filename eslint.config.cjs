5ad29e17b89d3a65ba4d132058c18c2a06ea198e
YoshiokaY <yoshioka.y@nadia.bz>
2024-09-26 17:11:12 +0900

    [update]eslint

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,17 +1,21 @@
-const js = require("@eslint/js");
 const prettier = require("eslint-config-prettier");
 
 module.exports = [
-  js.configs.recommended,
   {
     files: ["**/*.js"],
     ignores: ["**/*.config.js"],
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: "module",
-    },
-    parserOptions: {
-      sourceType: "module",
+      globals: {
+        browser: true,
+        node: true,
+        commonjs: true,
+        jquery: true,
+      },
+      parserOptions: {
+        sourceType: "module",
+      },
     },
     rules: {
       "no-console": "warn",
