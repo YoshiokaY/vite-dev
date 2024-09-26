9757cf076d6bd7631060d206cdba5902ba853ada
YoshiokaY <yoshioka.y@nadia.bz>
2024-09-26 16:59:50 +0900

    [update]eslint

--- .eslint.config.cjs
+++ .eslint.config.cjs
@@ -1,16 +1,21 @@
-module.exports = {
-  extends: ["next/core-web-vitals", "prettier"],
-  env: {
-    browser: true,
-    node: true,
-    commonjs: true,
-    es2021: true,
-    jquery: true,
+const js = require("@eslint/js");
+const prettier = require("eslint-config-prettier");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    ignores: ["**/*.config.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    parserOptions: {
+      sourceType: "module",
+    },
+    rules: {
+      "no-console": "warn",
+    },
   },
-  parserOptions: {
-    sourceType: "module",
-  },
-  rules: {
-    "no-console": "warn",
-  },
-};
+  prettier,
+];
