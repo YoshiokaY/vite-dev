45b6579a1e634d0bf7e04d20417b2244f8350167
YoshiokaY <yoshioka.y@nadia.bz>
2024-10-15 18:12:24 +0900

    [update]スクリプト名変更

--- check/check.auto.ts
+++ check/check.auto.ts
@@ -2,8 +2,6 @@ import type { Page } from "@playwright/test";
 import { test, expect } from "@playwright/test";
 import AxeBuilder from "@axe-core/playwright";
 import { createHtmlReport } from "axe-html-reporter";
-import sharp from "sharp";
-import { existsSync, mkdirSync } from "fs";
 import { pages, localhost, WCAG, disableRules } from "../check.config";
 
 interface TargetPage {
@@ -11,16 +9,8 @@ interface TargetPage {
   path: string;
 }
 
-// デザイン画像が格納しているディレクトリ
-const DESIGN_DIR = "./check/design/";
-// スクリーンショットを格納するディレクトリ
-const SCREENSHOT_DIR = "./check/screenshot/";
-// 差分ファイルを出力する格納するディレクトリ
-const DIFF_DIR = "./check/diff/";
 // a11yファイルを出力する格納するディレクトリ
 const a11y_DIR = "./check/a11y/";
-// 画像の拡張子
-const UNIT = ".png";
 // ページ読み込みの待機時間
 const TIME = 2000;
 
@@ -31,15 +21,9 @@ for (const targetPage of targetPages) {
     await test.step("コンソールエラーチェック", async () => {
       await consoleCheck(page, targetPage);
     });
-    // await test.step("ピクセルパーフェクト", async () => {
-    //   await pixelPerfect(page, targetPage);
-    // });
     await test.step("アクセシビリティチェック", async () => {
       await a11y(page, targetPage);
     });
-    // await test.step("ヴィジュアルリグレッション", async () => {
-    //   await vrtCheck(page, targetPage);
-    // });
   });
 }
 
@@ -60,71 +44,6 @@ const a11y = async (page: Page, targetPage: TargetPage) => {
   });
   // エラーがあれば失敗する
   expect.soft(results.violations).toEqual([]);
-};
-
-// VRT（前回との比較）
-const vrtCheck = async (page: Page, targetPage: TargetPage) => {
-  // test.configで設定したチェック対象のページを参照
-  await page.goto(localhost + targetPage.path, { waitUntil: "load" });
-  await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
-  await page.waitForTimeout(TIME); // スクロール完了までwait
-
-  // 前回と今回のスクリーンショットの比較を行う
-  await expect(page).toHaveScreenshot({ fullPage: true, animations: "disabled" });
-};
-
-// デザインチェック
-const pixelPerfect = async (page: Page, targetPage: TargetPage) => {
-  // 出力先フォルダがなければ作成
-  if (!existsSync(DIFF_DIR)) {
-    mkdirSync(DIFF_DIR);
-  }
-
-  // デザイン画像参照
-  const designFilePath = DESIGN_DIR + targetPage.name + UNIT;
-  const designImage = sharp(designFilePath);
-
-  await page.goto(localhost + targetPage.path, { waitUntil: "load" });
-
-  // ページの一番下までスクロールする
-  await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
-  await page.waitForTimeout(TIME);
-
-  // スクリーンショット撮影
-  const screenshot = await page.screenshot({ path: SCREENSHOT_DIR + targetPage.name + UNIT, fullPage: true, animations: "disabled" });
-  const screenshotImage = sharp(screenshot);
-
-  // スクリーンショットのサイズを取得
-  const ssImageMetaData = await screenshotImage.metadata();
-  const ssImageSize = {
-    width: ssImageMetaData.width,
-    height: ssImageMetaData.height,
-  };
-
-  // スクリーンショットに合わせてデザイン画像をリサイズ
-  const resizedImage = await designImage.resize({
-    width: ssImageSize.width,
-    position: "left top",
-    withoutEnlargement: true,
-  });
-
-  // デザイン画像とスクリーンショット画像を重ねて差の絶対値で差分を検出
-  const difference = await resizedImage
-    .composite([
-      {
-        input: screenshot,
-        blend: "difference",
-        gravity: "northwest",
-      },
-    ])
-    .toBuffer();
-
-  // ネガ反転してファイル出力
-  await sharp(difference)
-    .negate({ alpha: false })
-    .toFile(DIFF_DIR + "diff-" + targetPage.name + UNIT);
-
-  await test.info().attach("screenshot", { body: difference, contentType: "image/png" });
 };
 
 // コンソールエラー
